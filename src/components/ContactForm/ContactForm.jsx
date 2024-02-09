757329b7333852f7c389f964e2523993e492e1eb
Iryna Holova <gis.golova@gmail.com>
2024-02-09 20:27:51 +0200

    fix input bug

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -49,7 +49,7 @@ class ContactForm extends Component {
             id="name"
             onChange={this.handleChange}
             ref={this.nameInput}
-            placeholder=""
+            placeholder="Name"
             pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
@@ -71,7 +71,7 @@ class ContactForm extends Component {
             value={number}
             id="number"
             onChange={this.handleChange}
-            placeholder=""
+            placeholder="Number"
             pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
