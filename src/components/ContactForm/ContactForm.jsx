12fea87ef75857a3636c42a1fc1304f61ab8dc04
Iryna Holova <gis.golova@gmail.com>
2024-02-09 19:04:30 +0200

    change input styles

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -35,9 +35,9 @@ class ContactForm extends Component {
     const { name, number } = this.state;
     return (
       <form className={css.form} onSubmit={this.handleSubmit}>
-        <label className={css.label}>
+        <div className={css.field}>
           <IconContext.Provider
-            value={{ size: '16px', className: css.label_icon }}
+            value={{ size: '16px', className: css.field_icon }}
           >
             <FiUser />
           </IconContext.Provider>
@@ -46,17 +46,21 @@ class ContactForm extends Component {
             type="text"
             name="name"
             value={name}
+            id="name"
             onChange={this.handleChange}
             ref={this.nameInput}
-            placeholder="Enter name"
+            placeholder=""
             pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
-        </label>
-        <label className={css.label}>
+          <label className={css.label} for="name">
+            Name
+          </label>
+        </div>
+        <div className={css.field}>
           <IconContext.Provider
-            value={{ size: '16px', className: css.label_icon }}
+            value={{ size: '16px', className: css.field_icon }}
           >
             <FiPhone />
           </IconContext.Provider>
@@ -65,13 +69,18 @@ class ContactForm extends Component {
             type="tel"
             name="number"
             value={number}
+            id="number"
             onChange={this.handleChange}
-            placeholder="Enter number"
+            placeholder=""
             pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
           />
-        </label>
+          <label className={css.label} for="number">
+            Number
+          </label>
+        </div>
+
         <button className={css.add_button} type="submit">
           Add contact
         </button>
